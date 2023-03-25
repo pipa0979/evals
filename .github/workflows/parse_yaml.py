a27d17ce7b498c7c431f265f414362cb96b3ad68
Piyush Patel <piyush.patel@colorado.edu>
2023-03-25 15:59:23 -0700

    Update parse_yaml.py
    Adds error handling: The current implementation assumes that the file exists and is a valid YAML file. should add error handling to check for these conditions and raise appropriate exceptions, Adding a docstring to the get_first_key function would help other developers understand what it does and how to use it.
    
    Added the The with statement ensures that the file is properly closed, even if an exception occurs. So, you could consider using it when opening the YAML file.

--- .github/workflows/parse_yaml.py
+++ .github/workflows/parse_yaml.py
@@ -1,12 +1,35 @@
 import sys
 import yaml
 
+
 def get_first_key(file_path):
+    """
+    Returns the first key of a YAML file.
+
+    Args:
+        file_path (str): Path to the YAML file.
+
+    Returns:
+        str: The first key of the YAML file.
+    """
     with open(file_path, 'r') as yaml_file:
         content = yaml.safe_load(yaml_file)
+        if not isinstance(content, dict):
+            raise ValueError("Invalid YAML file.")
+        if not content:
+            raise ValueError("Empty YAML file.")
         first_key = next(iter(content))
         return first_key
 
+
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python get_first_key.py <yaml_file_path>")
+        sys.exit(1)
     yaml_file_path = sys.argv[1]
-    print(get_first_key(yaml_file_path))
+    try:
+        first_key = get_first_key(yaml_file_path)
+        print(first_key)
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        sys.exit(1)
